f2f4f9328c1327ac106394620446efb46080d110
Ebrahim Alshaker <alshaker@proton.me>
2026-01-02 07:31:03 +0300

    :wrench: feat: Add configuration variables for the application

---
+++ apps/frontendservice/albayanfrontapi/configs/config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+// config dictionary with populated values from OS ENV
+const config = {
+  PORT: parseInt(process.env.PORT, 10) || 3000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  AWS_REGION: process.env.AWS_REGION,
+  DYNAMODB_ENDPOINT: process.env.DYNAMODB_ENDPOINT,
+  DEFINITION_TABLE: process.env.DEFINITION_TABLE || "reports_definition",
+  PROCESSING_TABLE: process.env.PROCESSING_TABLE || "reports_processing",
+  WORKER_URL: process.env.WORKER_URL || "http://localhost:8000",
+  UPLOAD_FOLDER: process.env.UPLOAD_FOLDER,
+};
+
+// Export config dictionary
+export { config };
