6d35c6db12eb0bfd24a4afd0cd308555353356a5
Ebrahim Alshaker <alshaker@proton.me>
2026-01-02 12:30:14 +0300

    :loud_sound: feat: Improve startup logging

--- apps/frontendservice/server.js
+++ apps/frontendservice/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Enable JSON parsing for incoming requests
 app.use(express.json());
 // Serve files from that folder
-app.use(express.static(path.resolve(config.REPORT_OUTPUT_FOLDER)));
+app.use("/output", express.static(path.resolve(config.REPORT_OUTPUT_FOLDER)));
 // Morgan logging middleware
 app.use(morgan("combined"));
 // Application Routes
@@ -18,11 +18,22 @@ app.use(ReportCreationRoute);
 app.use(ReportsDefinitionRouter);
 // Only start the server when not running tests. Jest sets NODE_ENV=test.
 if (process.env.NODE_ENV != "test") {
-  app.listen(config.PORT, () =>
+  app.listen(config.PORT, () => {
     console.log(
       `🌐 The server is running in ${config.NODE_ENV} mode on port ${config.PORT}`
-    )
-  );
+    );
+    console.log(
+      `📁 Generated reports folder location is ${config.REPORT_OUTPUT_FOLDER}`
+    );
+    console.log(`📁 Reports templates location is ${config.UPLOAD_FOLDER}`);
+    console.log(
+      `🛢️  Dynamodb definition table is ${config.DEFINITION_TABLE} and processing table is ${config.PROCESSING_TABLE}`
+    );
+    console.log(`📑 Dynamodb URL ${config.DYNAMODB_ENDPOINT}`);
+    console.log(
+      `☁️  AWS Region ${config.AWS_REGION},🪪  AWS Access Key ${config.AWS_ACCESS_KEY_ID},🔑 AWS Key ${config.AWS_SECRET_ACCESS_KEY} `
+    );
+  });
 }
 
 export default app;
