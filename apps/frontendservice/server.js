aab2974c69571ff401d0b805ed8c252da7a27888
Ebrahim Alshaker <alshaker@proton.me>
2026-01-02 07:23:59 +0300

    :nicktie: feat: Albayanfrontapi starting point

---
+++ apps/frontendservice/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import { config } from "./albayanfrontapi/configs/config.js";
+import { ReportCreationRoute } from "./albayanfrontapi/routes/ReportCreation.route.js";
+import { ReportsDefinitionRouter } from "./albayanfrontapi/routes/ReportDefinition.route.js";
+
+// Initilize Express application
+const app = express();
+// Enable JSON parsing for incoming requests
+app.use(express.json());
+
+// Morgan logging middleware
+app.use(morgan("combined"));
+// Application Routes
+app.use(ReportCreationRoute);
+app.use(ReportsDefinitionRouter);
+// Only start the server when not running tests. Jest sets NODE_ENV=test.
+if (process.env.NODE_ENV != "test") {
+  app.listen(config.PORT, () =>
+    console.log(
+      `🌐 The server is running in ${config.NODE_ENV} mode on port ${config.PORT}`
+    )
+  );
+}
+
+export default app;
